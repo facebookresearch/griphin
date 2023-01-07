a1adafdbf08849681832168391fb3555c1a93c45
hongkuan <hongkuaz@usc.edu>
2023-01-06 16:05:30 -0800

    add argparse

--- frontend/run.py
+++ frontend/run.py
@@ -1,6 +1,21 @@
 import os
-import time
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_machine', type=int, default=4, help='number of machines (simulated as processes)')
+parser.add_argument('--num_root', type=int, default=8192, help='number of root nodes in each machine')
+parser.add_argument('--walk_length', type=int, default=15, help='walk length')
+parser.add_argument('--worker_name', type=str, default='worker{}', help='name of workers, formatted by rank')
+parser.add_argument('--file_path', type=str, default='engine/ogbn_files_txt_small', help='path to dataset')
+args = parser.parse_args()
+
+# NUM_MACHINES = 4
+# NUM_ROOTS = 8192
+# WALK_LENGTH = 15
+# WORKER_NAME = 'worker{}'
+# FILE_PATH = 'engine/ogbn_files_txt_small'
+
+import time
 import torch
 import torch.multiprocessing as mp
 import torch.distributed.rpc as rpc
@@ -9,11 +24,6 @@ from torch.distributed.rpc import RRef, remote
 from graph import GraphShard
 from random_walk import random_walk
 
-NUM_MACHINES = 4
-NUM_ROOTS = 8192
-WALK_LENGTH = 15
-WORKER_NAME = 'worker{}'
-FILE_PATH = 'engine/ogbn_files_txt_small'
 
 
 def run(rank):
@@ -21,13 +31,13 @@ def run(rank):
     os.environ['MASTER_PORT'] = '29500'
     options = rpc.TensorPipeRpcBackendOptions(num_worker_threads=4)
 
-    rpc.init_rpc(WORKER_NAME.format(rank), rank=rank, world_size=NUM_MACHINES, rpc_backend_options=options)
+    rpc.init_rpc(args.worker_name.format(rank), rank=rank, world_size=args.num_machine, rpc_backend_options=options)
 
     if rank == 0:
         rrefs = []
-        for machine_rank in range(NUM_MACHINES):
-            info = rpc.get_worker_info(WORKER_NAME.format(machine_rank))
-            rrefs.append(remote(info, GraphShard, args=(FILE_PATH, machine_rank)))
+        for machine_rank in range(args.num_machine):
+            info = rpc.get_worker_info(args.worker_name.format(machine_rank))
+            rrefs.append(remote(info, GraphShard, args=(args.file_path, machine_rank)))
 
         tik_ = time.time()
 
@@ -37,7 +47,7 @@ def run(rank):
                 rpc.rpc_async(
                     rref.owner(),
                     random_walk,
-                    args=(rrefs, NUM_MACHINES, NUM_ROOTS, WALK_LENGTH)
+                    args=(rrefs, args.num_machine, args.num_root, args.walk_length)
                 )
             )
 
@@ -56,7 +66,7 @@ if __name__ == '__main__':
     print('Spawn Multi-Process to simulate Multi-Machine scenario')
 
     tik = time.time()
-    mp.spawn(run, nprocs=NUM_MACHINES, join=True)
+    mp.spawn(run, nprocs=args.num_machine, join=True)
     tok = time.time()
 
     print(f'Outer Execution time = {tok - tik:.3}s')
