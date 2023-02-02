13f70fd09589bb79119ac3cff2343fa0bb73bd41
hongkuan <hongkuaz@usc.edu>
2023-02-02 13:12:45 -0800

    .

--- frontend/graph.py
+++ frontend/graph.py
@@ -14,7 +14,7 @@ def init_graph(path, shard_id):
     ids_file = 'p{}_ids.txt'
     shards_file = 'p{}_halo_shards.txt'
     csr_indices_file = 'csr_indices{}.txt'
-    csr_shard_indices_file = 'csr_shards{}.txt，'
+    csr_shard_indices_file = 'csr_shards{}.txt'
     csr_indptrs_file = 'csr_indptr{}.txt'
     partition_book = osp.join(path, 'partition_book.txt')
 
