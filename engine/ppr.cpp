01b7e62be33d145242632fc46f90a0a62783728b
farukakgul <omerfarukakgul1999@gmail.com>
2023-02-09 10:26:29 -0800

    ssppr

--- engine/ppr.cpp
+++ engine/ppr.cpp
@@ -7,10 +7,106 @@ PPR::PPR(VertexType targetId_, ShardType shardId_, float alpha_, float epsilon_)
     epsilon = epsilon_;
 
     r[std::make_pair(targetId, shardId)] = 1.;
-    activatedNodes.push_back(std::make_pair(targetId, shardId));
-    nextNodeIds.push_back(targetId);
-    nextShardIds.push_back(shardId);
+    activatedNodes[std::make_pair(targetId, shardId)] = std::make_tuple(targetId, shardId);
+    // nextNodeIds.push_back(targetId);
+    // nextShardIds.push_back(shardId);
 }
+
+VertexType PPR::getTargetId(){
+    return targetId;
+}
+
+ShardType PPR::getShardId(){
+    return shardId;
+}
+
+std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> PPR::getP(){
+    std::vector<VertexType> nodeIds;
+    std::vector<ShardType> shardIds;
+    std::vector<float> values;
+
+    for(std::map<std::pair<VertexType,ShardType>>, float>::iterator it = p.begin(); it != p.end(); ++it) {
+        nodeIds.push_back(std::get<0>(it->first));
+        shardIds.push_back(std::get<1>(it->first));
+        values.push_back(it->second);
+    }
+
+    int64_t len = nodeIds.size();
+
+    auto opts = torch::TensorOptions().dtype(torch::kInt32);
+    torch::Tensor torchNodeIds = torch::from_blob(nodeIds.data(), {len}, opts);
+    torch::Tensor torchShardIds = torch::from_blob(shardIds.data(), {len}, opts);
+
+    auto opts = torch::TensorOptions().dtype(torch::kFloat32);
+    torch::Tensor torchValues = torch::from_blob(values.data(), {len}, opts);
+
+    return std::make_tuple(torchNodeIds, torchShardIds, torchValues);
+}
+
+std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> PPR::getR(){
+    std::vector<VertexType> nodeIds;
+    std::vector<ShardType> shardIds;
+    std::vector<float> values;
+
+    for(std::map<std::pair<VertexType,ShardType>>, float>::iterator it = r.begin(); it != r.end(); ++it) {
+        nodeIds.push_back(std::get<0>(it->first));
+        shardIds.push_back(std::get<1>(it->first));
+        values.push_back(it->second);
+    }
+
+    int64_t len = nodeIds.size();
+
+    auto opts = torch::TensorOptions().dtype(torch::kInt32);
+    torch::Tensor torchNodeIds = torch::from_blob(nodeIds.data(), {len}, opts);
+    torch::Tensor torchShardIds = torch::from_blob(shardIds.data(), {len}, opts);
+
+    auto opts = torch::TensorOptions().dtype(torch::kFloat32);
+    torch::Tensor torchValues = torch::from_blob(values.data(), {len}, opts);
+
+    return std::make_tuple(torchNodeIds, torchShardIds, torchValues);
+}
+
+std::tuple<torch::Tensor, torch::Tensor> PPR::getActivatedNodes(){
+    std::vector<VertexType> nodeIds;
+    std::vector<ShardType> shardIds;
+
+    for(std::map<std::pair<VertexType, ShardType>, std::tuple<VertexType, ShardType>>::iterator it = activatedNodes.begin(); it != activatedNodes.end(); ++it) {
+        nodeIds.push_back(std::get<0>(it->second));
+        shardIds.push_back(std::get<1>(it->second));
+    }
+
+    int64_t len = nodeIds.size();
+    auto opts = torch::TensorOptions().dtype(torch::kInt32);
+
+    torch::Tensor torchNodeIds = torch::from_blob(nodeIds.data(), {len}, opts);
+    torch::Tensor torchShardIds = torch::from_blob(shardIds.data(), {len}, opts);
+
+    return std::make_tuple(torchNodeIds, torchShardIds);
+}
+
+
+std::tuple<torch::Tensor, torch::Tensor> PPR::pop_activated_nodes(){
+    std::vector<VertexType> nodeIds;
+    std::vector<ShardType> shardIds;
+
+    for(std::map<std::pair<VertexType, ShardType>, std::tuple<VertexType, ShardType>>::iterator it = activatedNodes.begin(); it != activatedNodes.end(); ++it) {
+        nodeIds.push_back(std::get<0>(it->second));
+        shardIds.push_back(std::get<1>(it->second));
+    }
+
+    activatedNodes.clear();
+    //nextNodeIds.clear();
+    //nextShardIds.clear();
+
+    int64_t len = nodeIds.size();
+    auto opts = torch::TensorOptions().dtype(torch::kInt32);
+
+    torch::Tensor torchNodeIds = torch::from_blob(nodeIds.data(), {len}, opts);
+    torch::Tensor torchShardIds = torch::from_blob(shardIds.data(), {len}, opts);
+
+    return std::make_tuple(torchNodeIds, torchShardIds);
+}
+
 
 void PPR::push(std::vector<std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>> neighborInfos_, torch::Tensor v_ids_, torch::Tensor v_shard_ids_){
     VertexType size = neighborInfos_.size();
@@ -25,17 +121,22 @@ void PPR::push(std::vector<std::tuple<torch::Tensor, torch::Tensor, torch::Tenso
 
         auto vKey = std::make_pair(vId, vShardId);
         p[vKey] += alpha * r[vKey];
-        torch::Tensor uVals = (1 - alpha) * r[vKey] * uWeights / // uDegrees.sum()
+        torch::Tensor uVals = (1 - alpha) * r[vKey] * uWeights / uWeights.sum(std::vector<int64_t>({0, uWeights.sizes()[0]}))
         r[vKey] = 0.;
+        activatedNodes.erase(std::find(activatedNodes.begin(),activatedNodes.end(), vKey));
 
         auto uSize = uIds.sizes()[0];
+
         for(int j = 0; j < uSize; j++){
             auto val = uVals[j];
             auto uId = uIds[j];
-            auto uShardId = uIds[j];
+            auto uShardId = uShardIds[j];
             auto uDegree = uDegrees[j];
 
             auto uKey = std::make_pair(uId, uShardId);
+
+            if(j == 0)
+                r[uKey] = 0;
 
             r[uKey] += val;
 
--- engine/ppr.h
+++ engine/ppr.h
@@ -16,12 +16,20 @@ class PPR{
         float epsilon;
         std::map<std::pair<VertexType,ShardType>>, float> p;
         std::map<std::pair<VertexType,ShardType>>, float> r;
-        std::vector<std::pair<VertexType,ShardType>> activatedNodes;
-        std::vector<VertexType> nextNodeIds;
-        std::vector<ShardType> nextShardIds;
+        std::map<std::pair<VertexType, ShardType>, std::tuple<VertexType, ShardType>> activatedNodes;
+        // std::vector<VertexType> nextNodeIds;
+        // std::vector<ShardType> nextShardIds;
 
     public:
         PPR(VertexType targetId_, ShardType shardId_, float alpha_, float epsilon_);
+
+        VertexType getTargetId();
+        ShardType getShardId();
+        std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> getP();
+        std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> getR();
+        std::tuple<torch::Tensor, torch::Tensor> getActivatedNodes();
+
+        std::tuple<torch::Tensor, torch::Tensor> pop_activated_nodes();
         void push(std::vector<std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>> neighborInfos_, torch::Tensor v_ids_, torch::Tensor v_shard_ids_);
 };
 
